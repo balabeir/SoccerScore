0ee26420d83e48c96c3857ce3bd9bf2c79aa4039
balabeir <beer32853@gmail.com>
2021-04-26 20:31:14 +0700

    front-end

---
+++ front-end/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Stencil Std, fantasy;
+  font-size: 60px;
+  text-align: center;
+  color: cadetblue;
+}
+</style>
